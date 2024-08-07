1d1c4c5443e5050abfee067b22f0071869b8107b
Max poblete <max.poblete2905@gmail.com>
2024-08-06 23:01:58 -0400

    test copi assets

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Configuración global de pipes
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +25,7 @@ async function bootstrap() {
     }),
   );
 
+  // Habilitar CORS
   app.enableCors();
 
   await app.listen(3000);
